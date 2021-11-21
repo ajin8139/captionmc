89a97f8b2c0d0e588827892daddbf57dcfb2bcbf
ajin8139 <39217229+ajin8139@users.noreply.github.com>
2021-11-21 16:47:08 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -26,19 +26,12 @@ async def caption(client, message: Message):
 def get_file_id(msg: Message):
     if msg.media:
         for message_type in (
-            "photo",
-            "animation",
-            "audio",
             "document",
-            "video",
-            "video_note",
-            "voice",
             # "contact",
             # "dice",
             # "poll",
             # "location",
             # "venue",
-            "sticker"
         ):
             obj = getattr(msg, message_type)
             if obj:
